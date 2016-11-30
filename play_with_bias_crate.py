20c757718a416c9cf9af5544646488cd4d3dc005
Dominik Neise <neised@phys.ethz.ch>
2016-11-30 09:53:05 +0100

    initial

---
+++ play_with_bias_crate.py
@@ -0,0 +1,83 @@
+# IPython log file
+
+import numpy as np
+import serial
+import struct
+import time
+import pandas as pd
+from itertools import zip_longest
+
+from collections import namedtuple
+
+ser = serial.Serial('/dev/serial/by-id/usb-FTDI_UM245R_FTE03K4V-if00-port0')
+ser.timeout = 1
+commands = {'reset': 0, 'read': 1, 'set': 3}
+
+Answer = namedtuple('Answer', 'current, counter, status, errors, board')
+
+
+def grouper(iterable, num, fillvalue=None):
+    args = [iter(iterable)] * num
+    return zip_longest(*args, fillvalue=fillvalue)
+
+
+def make_send_bytes(cmd, board, channel, voltage):
+    word = 0
+    word |= (voltage & 0xfff) << 0
+    word |= (channel & 0x1f) << 12
+    word |= (board & 0xf) << (12+5)
+    word |= (cmd & 0x7) << (12+5+4)
+    return struct.pack('!I', word)[1:]
+
+
+def make_answer(string):
+    if not len(string) == 4:
+        string = b'\x00' + string
+    word = struct.unpack('!I', string)[0]
+    return Answer(
+        current=(word >> 8) & 0xfff,
+        counter=(word >> 20) & 0x7,
+        status=(word >> 23) & 0x1,
+        errors=(word >> 4) & 0xf,
+        board=word & 0xf
+        )
+
+
+def set_voltage_read_N_times(board, channel, voltage, num=1):
+    return set_voltage_M_times_read_N_times(board, channel, voltage, M=1, N=num-1)
+
+
+def set_voltage_M_times_read_N_times(board, channel, voltage, M=1, N=0):
+    cmds = (
+        M * make_send_bytes(commands['set'], board, channel, voltage)
+        + N * make_send_bytes(commands['read'], board, channel, 0)
+        )
+    start_time = time.time()
+    ser.write(cmds)
+    answer = b''
+    while not len(answer) == len(cmds):
+        answer += ser.read(len(cmds) - len(answer))
+    stop_time = time.time()
+
+    df = pd.DataFrame([
+        make_answer(answer[i*3:(i+1)*3]) for i in range(len(answer)//3)
+    ])
+    df['Time'] = pd.to_datetime(
+        np.linspace(start_time, stop_time, M+N) * 1e9,
+        unit='ns'
+        )
+    df['dac'] = voltage
+    df['channel'] = channel
+    df['board_'] = board
+
+    # change dtypes, so df is nice and small
+    df['current'] = df['current'].astype(np.uint16)
+    df['counter'] = df['counter'].astype(np.uint8)
+    df['status'] = df['status'].astype(np.bool)
+    df['errors'] = df['errors'].astype(np.uint8)
+    df['board'] = df['board'].astype(np.int8)
+    df['dac'] = df['dac'].astype(np.uint16)
+    df['channel'] = df['channel'].astype(np.uint8)
+    df['board_'] = df['board_'].astype(np.uint8)
+
+    return df
